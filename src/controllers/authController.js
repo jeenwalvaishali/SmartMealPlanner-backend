7dbe846b7c9d9f13d05abca97a5095060e4b2363
Vaishali Jeenwal <jeenwal.v@northeastern.edu>
2026-01-27 19:36:56 -0800

    Get User Info AuthController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -85,3 +85,22 @@ exports.login = async (req, res) => {
         res.status(500).json({ message: 'Server error' });
     }
 };
+
+exports.getMe = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await User.findById(userId).select('-password');
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Server error' });
+    }
+};
+
+
